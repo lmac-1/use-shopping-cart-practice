26fcda36bbd84507dd4d81529b7d4b3ed62d14ab
lmac-1 <macartney.lucy@gmail.com>
2023-02-07 15:18:31 -0500

    deleting another unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,8 @@
-import { useShoppingCart } from "use-shopping-cart";
 import Products from "@/components/Products";
 import { products as productData } from "../data/products";
 //import { DebugCart } from "use-shopping-cart";
 
 export default function Home() {
-  /* Gets the totalPrice and method for redirecting to Stripe */
-  const { totalPrice, redirectToCheckout, cartCount, clearCart } =
-    useShoppingCart();
-
   return (
     <>
       <Products productData={productData} />
