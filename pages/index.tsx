04a3610c64953377372f017c36a1478dbe756aab
lmac-1 <macartney.lucy@gmail.com>
2023-02-07 15:17:57 -0500

    deleting unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
 import { useShoppingCart } from "use-shopping-cart";
-import { formatCurrencyString } from "use-shopping-cart/core";
 import Products from "@/components/Products";
 import { products as productData } from "../data/products";
 //import { DebugCart } from "use-shopping-cart";
-import Image from "next/image";
 
 export default function Home() {
   /* Gets the totalPrice and method for redirecting to Stripe */
